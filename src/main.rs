c86ad4d4d2267ac17829d51b37a721cbeef7eb29
interstellarfrog <119659110+interstellarfrog@users.noreply.github.com>
2023-04-21 04:37:50 +0100

    Added Serial Printing That Is Used For Testing - cargo test - Will Run Tests And Print To Main PC Standard Out From OS

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,11 @@
 // So We Need To OverWrite This As We Do Not Have The OS We Are Coding One.
 // And We Do Not Have Access To the Rust runtime and crt0.
 #![no_main]
-
-mod vga_buffer;
-
+#![feature(custom_test_frameworks)] // Allows Us To Run Custom Tests
+#![test_runner(crate::test_runner)] // Defines The Test Runner Function
+// No Main Makes This Not Run As Behind The Scenes Main Is Called For Testing
+// So We Change The Name
+#![reexport_test_harness_main = "test_main"] 
 
 //###################
 // CROSS COMPILING
@@ -18,22 +20,77 @@ mod vga_buffer;
 // Target Build
 // To Avoid Linker Errors We want to Compile Our Code Using Our Custom Target.
 
+
+mod vga_buffer;
+mod serial;
+
+
 use core::panic::PanicInfo;
+use x86_64::instructions::port::Port;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32)
+    }
+}
 
 
-// This function is called on panic.
-// The Panic Info Contains Information About The Panic.
-// The ! means that this function will never return.
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+
+
+#[cfg(not(test))] // If Not In Test
+#[panic_handler] // This function is called on panic.
+fn panic(info: &PanicInfo) -> ! { // The Panic Info Contains Information About The Panic.
+    println!("{info}");
     loop {}
 }
+
+#[cfg(test)] // If Test
+#[panic_handler] // This function is called on panic.
+fn panic(info: &PanicInfo) -> ! { // The Panic Info Contains Information About The Panic.
+    serial_println!("[Failed]\n"); // Print To Main PC Standard Out
+    serial_println!("Error: {}\n", info); 
+    exit_qemu(QemuExitCode::Failed);
+    loop {}
+}
+
+
+
 // Dont Mangle The Name Of This Function.
 // Extern "C" means that this function uses the C calling convention.
 // This Function Should Be Called _start For LLVM.
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    println!("Hello, World{} This Is A Println!", "!");
-    print!("This Is A Print!");
+    #[cfg(test)]
+    test_main();
+
+    println!("Now Has Serial Print That Can Be Used With Cargo Test");
     loop {}
+}
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) { // Runs The Tests 
+    if tests.len() == 1 {
+        serial_println!("Running 1 Test")
+    } else {
+    println!("Running {} Tests", tests.len());
+    }
+    for test in tests {
+        test();
+    }
+    exit_qemu(QemuExitCode::Success);
+}
+
+#[test_case]
+fn trivial_assertion() {
+    serial_print!("trivial assertion... ");
+    assert_eq!(1, 1);
+    serial_println!("[ok]");
 }
---
+++ src/serial.rs
@@ -0,0 +1,31 @@
+use uart_16550::SerialPort;
+use spin::Mutex;
+use lazy_static::lazy_static;
+use core::fmt::Write;
+
+lazy_static! { // Only Init Once On First Use
+    pub static ref SERIAL1: Mutex<SerialPort> = { // Wrap In SpinLock
+        let mut serial_port = unsafe { SerialPort::new(0x3f8) }; // 0x3f8 Standard Port Addr For The First Serial Interface
+        serial_port.init();
+        Mutex::new(serial_port)
+    };
+}
+
+#[doc(hidden)]
+pub fn _print(args: ::core::fmt::Arguments) {
+    SERIAL1.lock().write_fmt(args).expect("Serial Print Failed!") // Gets Lock And Writes
+}
+
+#[macro_export]
+macro_rules! serial_print { // Prints To Main PC From OS Using Serial
+    ($($arg:tt)*) => { $crate::serial::_print(format_args!($($arg)*))
+    };
+}
+
+#[macro_export]
+macro_rules! serial_println { // Adds Newline And Calls Serial_print
+    () => ($crate::serial_print!("\n"));
+    ($fmt:expr) => ($crate::serial_print!(concat!($fmt, "\n")));
+    ($fmt:expr, $($arg:tt)*) => ($crate::serial_print!(
+        concat!($fmt, "\n"), $($arg)*));
+}
